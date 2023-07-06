6d04df8cc3a651ffcb5f3aa47e611cc7dffe669a
Eli Golding <eligolding@gmail.com>
2023-07-06 09:28:46 -0400

    move method refactor

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import socket
-import ssl
 import tkinter
 import tkinter.font
 from html_parser import HTMLParser, Text, Element
 from css_parser import CSSParser, style, cascade_priority
+from network import request
 
 WIDTH, HEIGHT = 800, 600
 SCROLL_STEP = 100
@@ -285,56 +284,6 @@ class BlockLayout:
         self.line = []
         max_descent = max([metric["descent"] for metric in metrics])
         self.cursor_y = baseline + 1.25 * max_descent
-
-
-def request(url):
-    scheme, url = url.split("://", 1)
-    assert scheme in ["http", "https"], \
-        "Unknown scheme {}".format(scheme)
-
-    host, path = url.split('/', 1)
-    path = '/' + path
-
-    s = socket.socket(
-        family=socket.AF_INET,
-        type=socket.SOCK_STREAM,
-        proto=socket.IPPROTO_TCP,
-    )
-
-    port = 80 if scheme == "http" else 443
-
-    if ":" in host:
-        host, port = host.split(":", 1)
-        port = int(port)
-
-    s.connect((host, port))
-
-    if scheme == "https":
-        ctx = ssl.create_default_context()
-        s = ctx.wrap_socket(s, server_hostname=host)
-
-    s.send("GET {} HTTP/1.0\r\n".format(path).encode('utf8') + "HOST: {}\r\n\r\n".format(host).encode('utf8'))
-
-    response = s.makefile("r", encoding="utf8", newline="\r\n")
-
-    statusline = response.readline()
-    version, status, explanation = statusline.split(" ", 2)
-    assert status == "200", "{}: {}".format(status, explanation)
-
-    headers = {}
-    while True:
-        line = response.readline()
-        if line == "\r\n": break
-        header, value = line.split(":", 1)
-        headers[header.lower()] = value.strip()
-
-    assert "transfer-encoding" not in headers
-    assert "content-encoding" not in headers
-
-    body = response.read()
-    s.close()
-
-    return headers, body
 
 
 def print_tree(node, indent=0):
---
+++ network.py
@@ -0,0 +1,52 @@
+import socket
+import ssl
+
+
+def request(url):
+    scheme, url = url.split("://", 1)
+    assert scheme in ["http", "https"], \
+        "Unknown scheme {}".format(scheme)
+
+    host, path = url.split('/', 1)
+    path = '/' + path
+
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    )
+
+    port = 80 if scheme == "http" else 443
+
+    if ":" in host:
+        host, port = host.split(":", 1)
+        port = int(port)
+
+    s.connect((host, port))
+
+    if scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=host)
+
+    s.send("GET {} HTTP/1.0\r\n".format(path).encode('utf8') + "HOST: {}\r\n\r\n".format(host).encode('utf8'))
+
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+    statusline = response.readline()
+    version, status, explanation = statusline.split(" ", 2)
+    assert status == "200", "{}: {}".format(status, explanation)
+
+    headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n": break
+        header, value = line.split(":", 1)
+        headers[header.lower()] = value.strip()
+
+    assert "transfer-encoding" not in headers
+    assert "content-encoding" not in headers
+
+    body = response.read()
+    s.close()
+
+    return headers, body
